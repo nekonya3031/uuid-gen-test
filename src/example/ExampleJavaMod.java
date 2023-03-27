5eea937d51816bac7a8a3ffd0011ee4c3542eb14
nekonya <63801085+nekonya3031@users.noreply.github.com>
2023-03-27 22:08:54 +0300

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -15,17 +15,24 @@ public class ExampleJavaMod extends Mod{
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
+        public ExampleJavaMod(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
+            for(int i=0;i<100000;i++)
+                Log.info(getUUID());
         });
+    }
+
+    public String getUUID(){
+        String uuid = "";
+            byte[] result = new byte[8];
+            new Rand().nextBytes(result);
+            uuid = new String(Base64Coder.encode(result));
+            return uuid;
+    }
     }
 
     @Override
